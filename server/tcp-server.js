a1ae43cb4d27004793aae977b96357cef09acb09
Sarthak-code360 <sarthakmishra3032002@gmail.com>
2024-12-28 12:54:57 +0530

    Server for raw data

--- server/tcp-server.js
+++ server/tcp-server.js
@@ -5,28 +5,9 @@ const PORT = 3000;
 const server = net.createServer((socket) => {
     console.log('Client connected!', socket.remoteAddress);
 
-    let buffer = '';
-
     socket.on('data', (data) => {
-        buffer += data.toString('hex');
-
-        // Check for a delimiter (e.g., newline '\n') to identify message boundaries
-        let delimiter = '\n';
-        let messages = buffer.split(delimiter);
-
-        // Process all complete messages, leave incomplete in the buffer
-        for (let i = 0; i < messages.length - 1; i++) {
-            const rawMessage = messages[i];
-            console.log('Raw HEX Data received:', rawMessage);
-
-            const jsonData = { rawData: rawMessage, timestamp: new Date().toISOString() };
-            console.log('Converted to JSON:', jsonData);
-
-            socket.write('Data received!');
-        }
-
-        // Retain the last incomplete part in the buffer
-        buffer = messages[messages.length - 1];
+        // Simply log the raw data received
+        console.log('Raw Data received:', data);
     });
 
     socket.on('end', () => {
