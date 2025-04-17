d1cc6c97aeb6f8e1b5aba4e507a298b286ed9d15
Sarthak-code360 <sarthakmishra3032002@gmail.com>
2025-04-17 12:11:16 +0530

    simplified the start file

--- proto-server/src/index.js
+++ proto-server/src/index.js
@@ -1,3 +1,11 @@
-const { startServer } = require("./server/tcpServer");
+const { startServer, registerAppBroadcast } = require("./tcp/tcpServer");
+const { startWebSocketServer } = require("./websocket/wsServer");
 
+// Start TCP server
 startServer();
+
+// Start WebSocket server and connect it to the HW broadcast
+startWebSocketServer((fullState) => {
+    // This callback is invoked when the app sends data to HW
+    registerAppBroadcast(fullState);
+});
